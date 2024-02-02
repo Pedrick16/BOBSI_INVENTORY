c6b8a592ecb6d37b981cab7ce91da3c8196f8001
nocsupport12 <nocsupport@bicol1.com>
2024-02-02 15:59:25 +0800

    changed

--- src/pages/dashboard/Dashboad.jsx
+++ src/pages/dashboard/Dashboad.jsx
@@ -1,50 +1,90 @@
-import React from 'react'
-import ScrollReveal from 'scrollreveal';
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './dashboard.scss'
-
+import React from "react";
+import ScrollReveal from "scrollreveal";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./dashboard.scss";
 
 const Dashboad = () => {
   useEffect(() => {
-    ScrollReveal().reveal('._dashboard', {
+    ScrollReveal().reveal("._dashboard", {
       duration: 2000,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       reset: true, // This resets the properties when scrolling down
-      origin: 'right', // You can change the origin as needed
-      distance: '70px', // You can adjust the distance
+      origin: "right", // You can change the origin as needed
+      distance: "70px", // You can adjust the distance
       opacity: 0,
       afterReveal: (domEl) => {
         // Optional: You can add custom logic after the reveal animation
-        console.log('Element revealed:', domEl);
+        console.log("Element revealed:", domEl);
       },
     });
-
-    
-
-
-  }, [])
+  }, []);
   return (
-    <div className='_dashboard h-[100%] grid grid-rows-[auto,1fr] px-10 pb-10 pt-5'>
+    <div className="_dashboard h-[100%] grid grid-rows-[auto,1fr] px-10 pb-10 pt-5">
       <div className="div">Dashboard</div>
 
-      <div className=' bg-slate-50 gap-10 grid grid-cols-3 grid-rows-3  mt-[1px] '>
-      <Link to='/sample' className="transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>User Account </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Broadband Plans </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Network </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Finance </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Security </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Reported Issues </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Settings </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Lorem </span> <span className='font-bold text-[25px]'></span></Link>
-      <Link to='/sample' className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer" ><span>Lorem </span> <span className='font-bold text-[25px]'></span></Link>
-     
+      <div className=" bg-slate-50 gap-10 grid grid-cols-3 grid-rows-3  mt-[1px] ">
+        <Link
+          to="/sample"
+          className="transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>User Account </span>{" "}
+          <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>Broadband Plans </span>{" "}
+          <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>Network </span> <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>REGIONS</span> <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>PROVINCES </span>{" "}
+          <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>MUNICIPALS </span>{" "}
+          <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>BARANGAY </span> <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>Lorem </span> <span className="font-bold text-[25px]"></span>
+        </Link>
+        <Link
+          to="/sample"
+          className=".transform scale-100 hover:scale-110 transition-transform duration-500 bg-white  shadow-xl flex flex-col items-center justify-center  cursor-pointer"
+        >
+          <span>Lorem </span> <span className="font-bold text-[25px]"></span>
+        </Link>
+      </div>
     </div>
-    </div>  
-  )
+  );
+};
 
-
-  
-}
-
-export default Dashboad
+export default Dashboad;
--- src/shared/components/partials/sidebar/Sidebar.jsx
+++ src/shared/components/partials/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './sidebar.scss'
+import React from "react";
+import "./sidebar.scss";
 import { RxDashboard } from "react-icons/rx";
 import { FaUserAlt } from "react-icons/fa";
 import { TbBuildingBroadcastTower } from "react-icons/tb";
@@ -9,10 +9,8 @@ import { VscSettings } from "react-icons/vsc";
 import { IoClose } from "react-icons/io5";
 
 import { FiMenu } from "react-icons/fi";
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,72 +20,97 @@ const Sidebar = () => {
     console.log(isMenuOpen);
   };
 
-  
-
   return (
-    <div className={`__sidebar ${isMenuOpen ? "__show" : "__hide"} w-[300px] h-screen bg-slate-200 grid grid-rows-[auto,auto,auto,1fr]`}>
-         
-          {isMenuOpen ? (<FiMenu size={50}  className='__icon cursor-pointer  text-slate-400'  onClick={handleIconClick}/> ) : (
-            < IoClose  size={50}  className='__icon cursor-pointer text-slate-400' onClick={handleIconClick} />
-          )}
+    <div
+      className={`__sidebar ${
+        isMenuOpen ? "__show" : "__hide"
+      } w-[300px] h-screen bg-slate-200 grid grid-rows-[auto,auto,auto,1fr]`}
+    >
+      {isMenuOpen ? (
+        <FiMenu
+          size={50}
+          className="__icon cursor-pointer  text-slate-400"
+          onClick={handleIconClick}
+        />
+      ) : (
+        <IoClose
+          size={50}
+          className="__icon cursor-pointer text-slate-400"
+          onClick={handleIconClick}
+        />
+      )}
 
-        
-       <div className='px-[20px] pt-[20px] '>
-       <img src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=334,fit=crop,q=95/m2WbBNyqvMInW7G9/bobsi-sample-web-YZ9naB97xzfo3OOg.jpg"  alt="" />
-       </div>
-       <div className="flex   justify-center pt-2 " >
-            <span className='text-center text-1xl font-bold text-[#050505]'>BOBSI INTRANET</span>
-       </div>
-       
-      
-            <ul className=' __menu-container text-center mt-5'>
-                <Link to='/dashboard' className='_li h-[60px] border-t border-r border-l border-b border-solid shadow-sm md:shadow-md flex  flex-row items-center ps-4'>
-                  <RxDashboard /><span className='ps-5'>Dashboard</span>
-                </Link>
-                <Link to='/dashboard' className='_li h-[60px]  shadow-xl  flex  flex-row items-center ps-4'>
-                 
-                  <FaUserAlt /><span className='ps-5'> User Accounts</span>
-            
-                </Link>
-                <Link to='/dashboard' className='_li h-[60px]  shadow-xl  flex  flex-row items-center ps-4'>
-                  
-                  <TbBuildingBroadcastTower /><span className='ps-5'> Broadband Plans</span>
-                </Link>
-              
-                <Link to='/dashboard' className='_li h-[60px]   shadow-xl flex  items-center ps-4'>
-              
-                  <BiSolidReport /><span  className='ps-5'>Network</span>
-               
-                </Link>
-                <li className='_li h-[60px] shadow-xl  flex  flex-row items-center ps-4'>
-               
-               <GrMoney />
-               <span className='ps-5'> Finance</span >
-               
-             </li>
-             <Link to='/dashboard' className='_li h-[60px] shadow-xl  flex  flex-row items-center ps-4'>
-               
-               <GrMoney />
-               <span className='ps-5'>Security</span >
-               
-             </Link>
+      <div className="px-[20px] pt-[20px] ">
+        <img
+          src="https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=334,fit=crop,q=95/m2WbBNyqvMInW7G9/bobsi-sample-web-YZ9naB97xzfo3OOg.jpg"
+          alt=""
+        />
+      </div>
+      <div className="flex   justify-center pt-2 ">
+        <span className="text-center text-1xl font-bold text-[#050505]">
+          BOBSI SECURENET
+        </span>
+      </div>
 
-                <Link to='/dashboard' className='_li h-[60px]   shadow-xl flex  items-center ps-4'>
-              
-                 <BiSolidReport /><span  className='ps-5'>Reported Issues</span>
-                  
-                </Link>
-                <Link to="/dashboard" className='_li h-[60px] shadow-xl  flex items-center flex-row ps-4 '>
-           
-                  <VscSettings /><span  className='ps-5'>Settings</span>
-                
-                  
-                </Link>
+      <ul className=" __menu-container text-center mt-5">
+        <Link
+          to="/dashboard"
+          className="_li h-[60px] border-t border-r border-l border-b border-solid shadow-sm md:shadow-md flex  flex-row items-center ps-4"
+        >
+          <RxDashboard />
+          <span className="ps-5">Dashboard</span>
+        </Link>
+        <Link
+          to="/dashboard"
+          className="_li h-[60px]  shadow-xl  flex  flex-row items-center ps-4"
+        >
+          <FaUserAlt />
+          <span className="ps-5"> User Accounts</span>
+        </Link>
+        <Link
+          to="/dashboard"
+          className="_li h-[60px]  shadow-xl  flex  flex-row items-center ps-4"
+        >
+          <TbBuildingBroadcastTower />
+          <span className="ps-5"> Broadband Plans</span>
+        </Link>
 
-            </ul>
-     
+        <Link
+          to="/dashboard"
+          className="_li h-[60px]   shadow-xl flex  items-center ps-4"
+        >
+          <BiSolidReport />
+          <span className="ps-5">Network</span>
+        </Link>
+        <li className="_li h-[60px] shadow-xl  flex  flex-row items-center ps-4">
+          <GrMoney />
+          <span className="ps-5"> Finance</span>
+        </li>
+        <Link
+          to="/dashboard"
+          className="_li h-[60px] shadow-xl  flex  flex-row items-center ps-4"
+        >
+          <GrMoney />
+          <span className="ps-5">Security</span>
+        </Link>
+
+        <Link
+          to="/dashboard"
+          className="_li h-[60px]   shadow-xl flex  items-center ps-4"
+        >
+          <BiSolidReport />
+          <span className="ps-5">Reported Issues</span>
+        </Link>
+        <Link
+          to="/dashboard"
+          className="_li h-[60px] shadow-xl  flex items-center flex-row ps-4 "
+        >
+          <VscSettings />
+          <span className="ps-5">Settings</span>
+        </Link>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
